c037ebfe496ed76d372935741f31382dd0e6d807
Volkan Yazici <vyazici@bol.com>
2018-05-03 14:49:53 +0200

    Suppress copyq output.

--- copyq.bash
+++ copyq.bash
@@ -3,7 +3,7 @@
 clip() {
 	local sleep_argv0="password store sleep on display $DISPLAY"
 	pkill -f "^$sleep_argv0" 2>/dev/null && sleep 0.5
-	copyq copy "$1" || die "Error: Could not copy data to the clipboard"
+	copyq copy "$1" >/dev/null 2>&1 || die "Error: Could not copy data to the clipboard"
 	(
 		( exec -a "$sleep_argv0" sleep "$CLIP_TIME" )
 		copyq -e "if (size() > 0) { select(0); } else { copy(''); }"
