8a03d4b1856fb457240b2c1ab218dcaaff474a51
Olegek <4539453@gmail.com>
2022-12-11 19:18:20 +0000

    Fix error msg and file descriptor number. (#2)

--- copyq.bash
+++ copyq.bash
@@ -2,12 +2,20 @@
 
 clip() {
 	local sleep_argv0="password store sleep on display $DISPLAY"
-	pkill -f "^$sleep_argv0" 2>/dev/null && sleep 0.5
-	copyq copy -- "$1" >/dev/null 2>&1 || die "Error: Could not copy data to the clipboard"
-	(
-		( exec -a "$sleep_argv0" sleep "$CLIP_TIME" )
+
+	pkill -f "^$sleep_argv0" 1>/dev/null && sleep 0.5
+
+	if ! copyq copy -- "$1" >/dev/null 2>&1; then
+		echo "Error: Could not copy data to the clipboard" >&2
+		exit 1
+	fi
+
+	( 
+		(exec -a "$sleep_argv0" sleep "$CLIP_TIME")
 		copyq -e "if (size() > 0) { select(0); } else { copy(''); }"
-	) 2>/dev/null & disown
+	) >/dev/null 2>&1 &
+	disown
+
 	echo "Copied $2 to clipboard using copyq. Will clear in $CLIP_TIME seconds."
 }
 
